3d7d2792a4a07987adb7ec5c652bffd6e071fd77
DAYUCS <dayub@chinasystems.com>
2018-06-29 16:37:07 +0800

    try calculation sequence

--- src/app/lc-detail/lc-detail.component.ts
+++ src/app/lc-detail/lc-detail.component.ts
@@ -20,16 +20,18 @@ export class LcDetailComponent implements OnInit {
       .subscribe(data => {
         console.log(JSON.stringify(data));
 
-        // Calculate working buffer
-        data.lc_001 = data.lc_amt + 200;
-        data.lc_002 = data.lc_002 + 'A';
+        // Calculate 'lc_balance + 200' before
+        data.lc_001 = data.lc_balance + 200;
+        this.lcForm.get('lc_001').setValue(data.lc_001, { emitEvent: false });
 
         // Calculate L/C Balance
         var tolerance = customers.find(value => (value.id == data.lc_customer_no)).tolerance;
         data.lc_balance = data.lc_amt * (1 + tolerance / 100);
-
-        // Set Form field value
         this.lcForm.get('lc_balance').setValue(data.lc_balance, { emitEvent: false });
+
+        // Calculate 'lc_balance' + 200 after
+        data.lc_002 = data.lc_balance + 200;
+        this.lcForm.get('lc_002').setValue(data.lc_002, { emitEvent: false });
       });
   }
 
@@ -40,10 +42,10 @@ export class LcDetailComponent implements OnInit {
     this.lcForm = this.fb.group({
       lc_ref_no: ['', { required: true }],
       lc_customer_no: 1,
-      lc_amt: 0.0,
-      lc_balance: [0.0, { readonly: true }],
-      lc_001: [0.0, { readonly: true }],
-      lc_002: 'Text'
+      lc_amt: 0,
+      lc_balance: [0, { readonly: true }],
+      lc_001: [0, { readonly: true }],
+      lc_002: [0, { readonly: true }]
     });
   }
 }
